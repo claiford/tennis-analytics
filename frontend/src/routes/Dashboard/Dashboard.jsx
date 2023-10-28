a9c58370e674df51b4c6db1b2f021f2f29bf5034
Clifford Lok <cliffordcljw@gmail.com>
2023-10-28 15:44:47 +0800

    dashboard layout done

--- frontend/src/routes/Dashboard/Dashboard.jsx
+++ frontend/src/routes/Dashboard/Dashboard.jsx
@@ -1,6 +1,18 @@
 const Dashboard = () => {
     return (
-        <h1>Dashboard</h1>
+        <div className='h-full flex flex-row gap-5 m-5'>
+            <div className='w-1/2 bg-white rounded-xl'>
+                Upcoming
+            </div>
+            <div className='h-full w-1/2 flex flex-col gap-5'>
+                <div className='flex-auto bg-white rounded-xl'>
+                    Friends
+                </div>
+                <div className='flex-auto bg-white rounded-xl'>
+                    Latest
+                </div>
+            </div>
+        </div>
     )
 };
 
