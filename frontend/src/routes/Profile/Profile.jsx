5f109989b60290c148ff2d6ffa74a8cc8202e4b5
Clifford Lok <cliffordcljw@gmail.com>
2023-10-28 16:25:18 +0800

    profile layout done

--- frontend/src/routes/Profile/Profile.jsx
+++ frontend/src/routes/Profile/Profile.jsx
@@ -1,6 +1,18 @@
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div className='h-full flex flex-row gap-5 m-5'>
+            <div className='w-1/2 bg-white rounded-xl'>
+                Profile
+            </div>
+            <div className='h-full flex-auto flex flex-col gap-5'>
+                <div className='h-1/3 bg-white rounded-xl'>
+                    Stats
+                </div>
+                <div className='flex-auto bg-white rounded-xl'>
+                    Friends
+                </div>
+            </div>
+        </div>
     )
 };
 
