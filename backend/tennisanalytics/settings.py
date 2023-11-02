1f61b3828a44d947fc65381d4dd6f2b1e85f105c
Clifford Lok <cliffordcljw@gmail.com>
2023-11-02 21:37:46 +0800

    apply fix for static files settings

--- backend/tennisanalytics/settings.py
+++ backend/tennisanalytics/settings.py
@@ -146,5 +146,3 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
