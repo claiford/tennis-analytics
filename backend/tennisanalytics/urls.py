8b59c7704ab425a98335713f728b73baa944bfeb
Clifford Lok <cliffordcljw@gmail.com>
2023-11-03 21:15:23 +0800

    deployment fix

--- backend/tennisanalytics/urls.py
+++ backend/tennisanalytics/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tennisanalytics import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,3 +37,5 @@ urlpatterns = [
     path('api/diagnostics/create', views.createDiagnostic),
     path('api/diagnostics/write', views.editDiagnosticNotes),
 ]
+
+urlpatterns += staticfiles_urlpatterns() # new
